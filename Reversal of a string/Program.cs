f6d98601e2d7e89941cd0b18a2652ca9e1375880
Roopha <roophashree@gmail.com>
2020-09-06 23:02:18 -0500

    String Reversal

---
+++ Reversal of a string/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Reversal_of_a_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myStr, rev;
+            myStr = "RoopaShree";
+            rev = "";
+            Console.WriteLine("String is {0}", myStr);
+            // find string length
+            int len;
+            len = myStr.Length - 1;
+            while (len >= 0)
+            {
+                rev = rev + myStr[len];
+                len--;
+            }
+            Console.WriteLine("Reversed String is {0}", rev);
+            Console.ReadLine();
+        }
+    }
+}
