16c450ecb03e1c0eb93351054ce35158cf583a12
Roopha <roophashree@gmail.com>
2020-09-10 18:57:51 -0500

    C# break

---
+++ Using_Break/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Using_Break
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 5)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
