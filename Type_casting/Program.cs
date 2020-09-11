6be719a0425fc3f0312e69b17ddfd3fd5689ac52
Roopha <roophashree@gmail.com>
2020-09-10 19:04:01 -0500

    Casting

---
+++ Type_casting/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Type_casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 9;
+            double myDouble = myInt;       // int to double
+
+            Console.WriteLine(myInt);      
+            Console.WriteLine(myDouble);
+        }
+    }
+}
