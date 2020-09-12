bdb3bed467ed67f475125c4477edf7ed291f47f9
Roopha <roophashree@gmail.com>
2020-09-11 20:19:28 -0500

    C#

---
+++ CSharp_Math/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_Math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Max(5, 10));
+        }
+    }
+}
---
+++ Math.Min/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Math.Min
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
