60f758bf9ba022746d72e9ed835d937bb770e04a
Roopha <roophashree@gmail.com>
2020-09-11 21:54:22 -0500

    Commit

--- CSharp_Math/Program.cs
+++ CSharp_Math/Program.cs
@@ -10,6 +10,7 @@ namespace CSharp_Math
             Console.WriteLine(Math.Min(5, 10));
             Console.WriteLine(Math.Sqrt(64));
             Console.WriteLine(Math.Abs(-4.7));
+            Console.WriteLine(Math.Round(9.99));
         }
     }
 }
