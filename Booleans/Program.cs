5484482b65a50a3ed48d04cc7ae01c5e850d1c07
Roopha <roophashree@gmail.com>
2020-09-12 23:26:53 -0500

    Bool

---
+++ Booleans/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Booleans
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isCSharpFun = true;
+            bool isFishTasty = false;
+            Console.WriteLine(isCSharpFun);   
+            Console.WriteLine(isFishTasty);
+        }
+    }
+}
