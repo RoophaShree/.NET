c615182f84d7d978184fbb3f89dc47c2f109f742
Roopha <roophashree@gmail.com>
2020-09-15 08:32:43 -0500

    continue

---
+++ Break_Continue/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Break_Continue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i =0;i<20;i++)
+            {
+                if (i == 3)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
