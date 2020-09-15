d83ffb99fe6aae0adcd424447351fbf2c2f5884d
Roopha <roophashree@gmail.com>
2020-09-14 22:39:42 -0500

    While stmt

---
+++ String_reversal/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Reversal_of_a_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myStr, rev;
+            int a = {1, 2, 3};
+            rev = "";
+            Console.WriteLine("Enter the string", myStr);
+        
+            int len;
+            len = myStr.Length - 1;
+            while (len >= 0)
+            {
+                rev = rev + myStr[len];
+                len--;
+            }
+            Console.WriteLine("Reversed String is {0}", rev);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ While_loop/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace While_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i=0;
+            while(i<20)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
