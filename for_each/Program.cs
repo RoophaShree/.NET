ad0abee05213ff6d46f9bb3056e7cf2017be7c36
Roopha <roophashree@gmail.com>
2020-09-14 23:00:23 -0500

    foreach

---
+++ for_each/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace for_each
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] grocery = { "milk", "chocolate", "flowers", "biscuits" };
+            foreach(string i in grocery)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
