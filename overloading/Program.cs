69eece3e1b37c193851d9a9d919ca1f12c81e0b4
Roopha <roophashree@gmail.com>
2020-09-16 18:33:13 -0500

    overloading

---
+++ overloading/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(addMethod(8, 6));
+            Console.WriteLine(addMethod(1, 2));
+        }
+
+        static int addMethod(int x,int y)
+        {
+            int m;
+            return m = x + y;
+        }
+        static double addMethod(double a, double b)
+        {
+            double n;
+            return n = a + b;
+        }
+    }
+}
