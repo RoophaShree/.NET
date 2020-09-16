f06446130bf35e8b7c437441a3875d5d6db69951
Roopha <roophashree@gmail.com>
2020-09-15 19:12:47 -0500

    With Parameters

---
+++ method_parameter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace method_parameter
+{
+    class Program
+    {
+        static void myMethod(string fname)
+        {
+            Console.WriteLine("Ryon " + fname);
+
+        }
+        static void Main(string[] args)
+        {
+            myMethod("Robin");
+            myMethod("Pascal");
+            myMethod("Ghays");
+        }
+    }
+}
