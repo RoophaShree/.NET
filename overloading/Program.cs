81290a62a9998818a6137b9948c55190304ddfef
Roopha <roophashree@gmail.com>
2020-09-16 18:39:20 -0500

    2 differnt classes

--- overloading/Program.cs
+++ overloading/Program.cs
@@ -8,18 +8,18 @@ namespace overloading
         {
             int Num1;
             double Num2;
-            Num1 = addMethod(8, 6);
-            Num2 = addMethod(1, 2);
+            Num1 = addMethodint(8, 6);
+            Num2 = addMethoddouble(1.2, 2.4);
             Console.WriteLine("Int: " +Num1);
             Console.WriteLine("double:" +Num2);
         }
 
-        static int addMethod(int x,int y)
+        static int addMethodint(int x,int y)
         {
             
             return  x + y;
         }
-        static double addMethod(double a, double b)
+        static double addMethoddouble(double a, double b)
         {
             
             return  a + b;
