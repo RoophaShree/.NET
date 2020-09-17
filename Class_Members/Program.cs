a15b735dac125215f1bdbb6cdbbb2108581416c2
Roopha <roophashree@gmail.com>
2020-09-17 17:59:35 -0500

    Multiple objects

---
+++ Class_Members/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Class_Members
+{
+    class Program
+    {
+        class Car
+        {
+            string model;
+            string color;
+            int year;
+
+            static void Main(string[] args)
+            {
+                Car Civic = new Car();
+                Civic.model = "Honda";
+                Civic.color = "white";
+                Civic.year = 2003;
+
+                Car Ford = new Car();
+                Ford.model = "Mustang";
+                Ford.color = "Yellow";
+                Ford.year = 2014;
+
+                Console.WriteLine(Civic.model);
+                Console.WriteLine(Ford.color);
+
+            }
+        }
+    }
+}
