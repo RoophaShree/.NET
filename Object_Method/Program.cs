31561bb44c976122eb1b4849fb9bfa408b79b204
Roopha <roophashree@gmail.com>
2020-09-17 18:11:48 -0500

    Calling the method

---
+++ Object_Method/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Object_Method
+{
+    class Car
+    {
+        string color="red";
+        string model = "2019";
+        public void carSpeed()
+        {
+            Console.WriteLine("The car is going in its highest speed");
+        }
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            Console.WriteLine(myObj.color);
+            myObj.carSpeed();
+        }
+    }
+}
