f8ffa0e93544d3a37928a33b79ad6ecd459b094d
Roopha <roophashree@gmail.com>
2020-09-17 18:39:36 -0500

    Constructor

---
+++ Constructor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Constructor
+{
+    class Car
+    {
+        public string color;
+
+        public Car()
+        {
+            color = "red";
+        }
+
+
+        static void Main(string[] args)
+        {
+            Car Ford = new Car();
+            Console.WriteLine(Ford.color);
+        }
+    }
+}
