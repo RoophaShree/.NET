81501729ddf45f3018aa300325ea8b6c3994f366
Roopha <roophashree@gmail.com>
2020-09-16 19:09:42 -0500

    multible objects

--- Class/Program.cs
+++ Class/Program.cs
@@ -8,8 +8,10 @@ namespace Class
         string color = "red";
         static void Main(string[] args)
         {
-            Car myobj = new Car();
-            Console.WriteLine(myobj.color);
+            Car myobj1 = new Car();
+            Car myobj2 = new Car();
+            Console.WriteLine(myobj1.color);
+            Console.WriteLine(myobj2.color);
         }
         
     }
