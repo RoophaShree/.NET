cabd31b0aa1d9dc7768d5e4966c1dfcd4631fb53
Roopha <roophashree@gmail.com>
2020-09-16 19:25:37 -0500

    using multiple classes

---
+++ Car/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Car
+{
+    class Car
+    {
+        public string color = "red";
+    }
+}
---
+++ Object/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            Console.WriteLine(myObj.color);
+        }
+    }
+}
