23d9ed8a95445a8cef2203606022549fdd56090f
Roopha <roophashree@gmail.com>
2020-09-17 20:46:30 -0500

    multiple parameters

--- Constructor/Program.cs
+++ Constructor/Program.cs
@@ -5,17 +5,21 @@ namespace Constructor
     class Car
     {
         public string model;
+        public string color;
+        public int speed;
         
-        public Car(string modelName)
+        public Car(string modelName, string modelColor, int modelSpeed)
         {
             model = modelName;
+            color = modelColor;
+            speed = modelSpeed;
            
      
         }
         static void Main(string[] args)
         {
-            Car Ford = new Car("Mustang");
-            Console.WriteLine(Ford.model);
+            Car Ford = new Car("Mustang","red",100);
+            Console.WriteLine(Ford.model+ " " +Ford.color+ " " +Ford.speed);
             
         }
     }
