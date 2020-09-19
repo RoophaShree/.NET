eb28d999b9e40486621dfa8a0cff4a63d1725534
Roopha <roophashree@gmail.com>
2020-09-18 20:41:53 -0500

    Private

---
+++ Access_Modifiers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Access_Modifiers
+{
+    class Car
+    {
+        private string model = "Ford";
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            Console.WriteLine(myObj.model);
+        }
+    }
+}
