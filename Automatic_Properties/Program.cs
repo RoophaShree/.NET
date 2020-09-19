ea40f4ba64b09253bc64fe87b3d1b48305edb1f7
Roopha <roophashree@gmail.com>
2020-09-18 21:28:30 -0500

    Automatic Property

---
+++ Automatic_Properties/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Automatic_Properties
+{
+    class Person
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person myObj = new Person();
+            myObj.Name = "Roopa";
+            Console.WriteLine(myObj.Name);
+        }
+    }
+}
