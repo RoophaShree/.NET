5b1586a953a12171c647e6a87310c0f081642846
Roopha <roophashree@gmail.com>
2020-09-18 21:01:19 -0500

    set and get methods

---
+++ Properties/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Properties
+{
+    class Person
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person myPerson = new Person();
+            myPerson.Name = "Roopa";
+            Console.WriteLine(myPerson.Name);
+        }
+    }
+}
