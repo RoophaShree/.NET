5a91af4052ecdc9f3e5920fda5f04240a6e0b74f
Roopha <roophashree@gmail.com>
2020-09-19 23:30:23 -0500

    Abstract

---
+++ Abstract/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Abstract
+{
+    abstract class Animal
+    {
+        public abstract void AnimalSound();
+        public void sleep()
+        {
+            Console.WriteLine("ZZZZzzzzzzzzzzzz");
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
