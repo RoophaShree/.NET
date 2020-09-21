9853652bd7e4a595ccd5a5cb91983ac5f5077a89
Roopha <roophashree@gmail.com>
2020-09-20 19:44:15 -0500

    Interface

---
+++ Interface/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Interface
+{
+
+    interface Animal
+    {
+        void animalSound(); 
+        void run(); 
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
