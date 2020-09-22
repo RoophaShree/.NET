573686ea357f862f64250efc9d9702c575284df7
Roopha <roophashree@gmail.com>
2020-09-21 20:18:02 -0500

    Multiple Interface

---
+++ Multiple_Interfaces/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Multiple_Interfaces
+{
+    interface IFirstInterface
+    {
+        void myMethod();
+    }
+
+    interface ISecondInterface
+    {
+        void myOtherMethod();
+    }
+
+    class DemoClass : IFirstInterface, ISecondInterface
+    {
+        public void myMethod()
+        {
+            Console.WriteLine("This is my inteface method");
+        }
+
+        public void myOtherMethod()
+        {
+            Console.WriteLine("This is my second interface");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DemoClass myObj = new DemoClass();
+            myObj.myMethod();
+            myObj.myOtherMethod();
+        }
+    }
+}
