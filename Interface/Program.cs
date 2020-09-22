464ebdca2411e19faa0d222931ebf5f0a5bad90f
Roopha <roophashree@gmail.com>
2020-09-21 19:18:26 -0500

    Interface

--- Interface/Program.cs
+++ Interface/Program.cs
@@ -3,16 +3,30 @@
 namespace Interface
 {
 
-    interface Animal
+    interface IAnimal
     {
         void animalSound(); 
         void run(); 
+    }
+    class Pig : IAnimal
+    {
+        public void animalSound()
+        {
+            Console.WriteLine("The Pig says: Oink Oink");
+        }
+
+        public void run()
+        {
+            Console.WriteLine("The pig runs very slow");
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Pig myPig = new Pig();
+            myPig.animalSound();
+            myPig.run();
         }
     }
 }
