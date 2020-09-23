583dc1c5a98b5c1a5bc70220529c5c4c980b7aa0
Roopha <roophashree@gmail.com>
2020-09-22 20:45:47 -0500

    Enum values

---
+++ Enum_values/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Enum_values
+{
+    enum Months
+    {
+        January,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myMonths = (int)Months.July;
+            Console.WriteLine(myMonths);
+        }
+    }
+}
