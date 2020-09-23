dfdb15db7bbd62adefb238d800db1aff1d2be6a5
Roopha <roophashree@gmail.com>
2020-09-22 23:52:50 -0500

    Files

---
+++ Files/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.IO;  
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string writeText = "Hello World!";  
+            File.WriteAllText("filename.txt", writeText);  
+
+            string readText = File.ReadAllText("filename.txt"); 
+            Console.WriteLine(readText);
+        }
+    }
+}
