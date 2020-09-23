235f43cefb73438b93e97c9e46a6e7a4ae3da116
Roopha <roophashree@gmail.com>
2020-09-22 20:51:47 -0500

    Switch

---
+++ Enum_Switch/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Enum_Switch
+{
+    enum Level
+    {
+        Low,
+        Medium,
+        High
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Level myLevel = Level.High;
+            switch(myLevel)
+            {
+                case Level.Low:
+                    Console.WriteLine("Low Level");
+                    break;
+
+                case Level.Medium:
+                    Console.WriteLine("Medium Level");
+                    break;
+
+                case Level.High:
+                    Console.WriteLine("High Level");
+                    break;
+            }
+        }
+    }
+}
