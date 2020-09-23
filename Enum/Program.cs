a9f553bf088f93299e7e6e2906335f61880e3475
Roopha <roophashree@gmail.com>
2020-09-22 20:34:53 -0500

    Enum

---
+++ Enum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Enum
+{
+    enum Level
+    {
+        Low,
+        Medium,
+        High
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Level myLevel = Level.Medium;
+
+            Console.WriteLine(myLevel);
+        }
+    }
+}
