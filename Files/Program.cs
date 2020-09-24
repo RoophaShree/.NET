a95096e1b5ab9ecebe2e46875cf87864ebf4db0a
Roopha <roophashree@gmail.com>
2020-09-23 21:25:32 -0500

    writeAllText

--- Files/Program.cs
+++ Files/Program.cs
@@ -8,8 +8,8 @@ namespace MyApplication
     {
         static void Main(string[] args)
         {
-            string writeAllBytes = "Hi RoophaShree, how are you?";
-            File.WriteAllBytes("fname.txt", writeAllBytes);
+            string writeAllText = "Hi RoophaShree, how are you?";
+            File.WriteAllText("fname.txt", writeAllText);
 
             string readText = File.ReadAllText("filename.txt"); 
             Console.WriteLine(readText);
