9575738b9e2f5188875d9a8a94718a9d1372381d
Roopha <roophashree@gmail.com>
2020-09-23 21:33:44 -0500

    No_Exception

---
+++ Exceptions/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myNum = { 1, 2, 3 };
+            Console.WriteLine(myNum[2]);
+        }
+    }
+}
