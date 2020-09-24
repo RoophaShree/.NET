36471a690d2ea3e1af18f4ecca65cc4be44737be
Roopha <roophashree@gmail.com>
2020-09-23 21:56:58 -0500

    Exception_granted

---
+++ Exception_handling/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Cache;
+
+namespace Exception_handling
+{
+    class Program
+    {
+        static void checkAge(int age)
+        {
+            if(age<18)
+            {
+                throw new ArithmeticException("Access Denied-You must be at least 18 years old");
+            }
+            else
+            {
+                Console.WriteLine("Access Granted-you are good to go");
+            }
+        }
+        static void Main(string[] args)
+        {
+            checkAge(18);
+        }
+    }
+}
