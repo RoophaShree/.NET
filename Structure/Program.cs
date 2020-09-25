a3955a34b562cfc1b03b1ed968721780d7e291be
Roopha <roophashree@gmail.com>
2020-09-24 21:42:24 -0500

    with +

--- Structure/Program.cs
+++ Structure/Program.cs
@@ -11,13 +11,13 @@ namespace Structure
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\n\n Declare a simple structure:\n");
+            Console.WriteLine("\n\nDeclare a simple structure:\n");
             Console.WriteLine("---------------------------------\n");
             structure st = new structure();
             st.x = 15;
             st.y = 20;
             int sum = st.x + st.y;
-            Console.WriteLine("The sum of x and y is: ", sum);
+            Console.WriteLine("The sum of x and y is " +sum);
         }
     }
 }
