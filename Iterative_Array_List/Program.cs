488712a38878a6f842ef87c94d03729a35933b97
Roopha <roophashree@gmail.com>
2020-09-27 20:02:56 -0500

    Using ArrayList

---
+++ Iterative_Array_List/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+public class Program
+{
+	public static void Main()
+	{
+		ArrayList arlist = new ArrayList()
+						{
+							1,
+							"Bill",
+							"Roopa",
+							"Vicky",
+							3.5F,
+						};
+
+		Console.WriteLine("foreach loop");
+		foreach (var item in arlist)
+			Console.Write(item + ", "); 
+
+		Console.WriteLine("\n");
+		Console.WriteLine("for loop");
+
+		for (int i = 0; i < arlist.Count; i++)
+			Console.Write(arlist[i] + ", ");
+	}
+}
