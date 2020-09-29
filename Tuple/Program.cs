4bc4e9d84b879b087cd118acf9ffc4dcc7b0b7f7
Roopha <roophashree@gmail.com>
2020-09-28 19:47:10 -0500

    Tuple items

---
+++ Tuple/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ppl = Tuple.Create(1, "Roopa", "Jobs");
+        Console.WriteLine(ppl.Item1);
+        Console.WriteLine(ppl.Item2);
+        Console.WriteLine(ppl.Item3);
+        }
+    }
+
