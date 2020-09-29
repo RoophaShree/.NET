f9448bd750e8eaac35b45459544a272cfb8b35be
Roopha <roophashree@gmail.com>
2020-09-28 19:11:19 -0500

    Queue

---
+++ Queue/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+            queue.Enqueue(6);
+
+            foreach (var num in queue)
+                Console.WriteLine(num);
+
+        }
+    }
+}
