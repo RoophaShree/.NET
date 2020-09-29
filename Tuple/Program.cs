21075cb6a6d881ca0467388277295de1125d395d
Roopha <roophashree@gmail.com>
2020-09-28 19:50:06 -0500

    Accessing Tuple Elements

--- Tuple/Program.cs
+++ Tuple/Program.cs
@@ -11,6 +11,10 @@ using System.Collections;
         Console.WriteLine(ppl.Item1);
         Console.WriteLine(ppl.Item2);
         Console.WriteLine(ppl.Item3);
+        Console.WriteLine("\n");
+
+        var numbers = Tuple.Create("One", 2, 3, "Four", 5, "Six", 7, "Eight");
+        Console.WriteLine(numbers.Item1);
         }
     }
 
