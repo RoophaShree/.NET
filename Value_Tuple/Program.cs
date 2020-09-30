cdd4b488ac651eb9e99f78778ed2733129d91e3b
Roopha <roophashree@gmail.com>
2020-09-29 23:01:38 -0500

    Nested Tuple values changed

--- Value_Tuple/Program.cs
+++ Value_Tuple/Program.cs
@@ -4,11 +4,15 @@ public class Program
 {
 	public static void Main()
 	{
-		var numbers = Tuple.Create(1, 2, 3, 4, 5, 6, 7, Tuple.Create(8, 9, 10, 11, 12, 13));
-		Console.WriteLine(numbers.Item1); 
-		Console.WriteLine(numbers.Item7); 
+		var numbers = Tuple.Create(1, 2, Tuple.Create(3, 4, 5, 6, 7, 8), 9, 10, 11, 12, 13);
+
+		Console.WriteLine(numbers.Item1);
+		Console.WriteLine(numbers.Item2);
+		Console.WriteLine(numbers.Item3); 
+		Console.WriteLine(numbers.Item3.Item1); 
+		Console.WriteLine(numbers.Item4); 
 		Console.WriteLine(numbers.Rest.Item1); 
-		Console.WriteLine(numbers.Rest.Item1.Item1); 
-		Console.WriteLine(numbers.Rest.Item1.Item2); 
+
+
 	}
 }
