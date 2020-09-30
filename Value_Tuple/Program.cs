b1b4b8bbd87c1c3e03ee070684899b15cd2403bc
Roopha <roophashree@gmail.com>
2020-09-29 22:59:33 -0500

    nested tuple

---
+++ Value_Tuple/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		var numbers = Tuple.Create(1, 2, 3, 4, 5, 6, 7, Tuple.Create(8, 9, 10, 11, 12, 13));
+		Console.WriteLine(numbers.Item1); 
+		Console.WriteLine(numbers.Item7); 
+		Console.WriteLine(numbers.Rest.Item1); 
+		Console.WriteLine(numbers.Rest.Item1.Item1); 
+		Console.WriteLine(numbers.Rest.Item1.Item2); 
+	}
+}
