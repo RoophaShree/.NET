f8a14f5883340b5a6aa0bd449b1718310ccd8917
Roopha <roophashree@gmail.com>
2020-09-30 22:43:46 -0500

    entering non numeric num

---
+++ Exception/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exception
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter a number: ");
+
+            var num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Squre of {0} is {1}", num, num * num);
+        }
+    }
+}
