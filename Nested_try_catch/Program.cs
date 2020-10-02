b4fb5ee8637791ef8f34e945cb130391ba118464
Roopha <roophashree@gmail.com>
2020-10-01 22:18:07 -0500

    Nested_try_catch

---
+++ Nested_try_catch/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		var divider = 0;
+
+		try
+		{
+			try
+			{
+				var result = 100 / divider;
+			}
+			catch
+			{
+				Console.WriteLine("Inner catch");
+			}
+		}
+		catch
+		{
+			Console.WriteLine("Outer catch");
+		}
+	}
+}
