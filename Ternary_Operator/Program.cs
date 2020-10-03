a6c7f9d41b162e465bac499882ecbee2819c1725
Roopha <roophashree@gmail.com>
2020-10-02 23:07:47 -0500

    Replacing ternary with if

--- Ternary_Operator/Program.cs
+++ Ternary_Operator/Program.cs
@@ -11,6 +11,13 @@ namespace Ternary_Operator
             var result = x > y ? "x is greater than y" : "x is less than or equal to y";
 
             Console.WriteLine(result);
+
+            int a = 20, b = 10;
+
+            if (x > y)
+                Console.WriteLine("a is greater than b");
+            else
+                Console.WriteLine("a is less than b");
         }
     }
 }
