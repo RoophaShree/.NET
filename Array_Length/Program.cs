ab907c3fd76937a70770b3b862e3fb3dc0084f5c
Roopha <roophashree@gmail.com>
2020-10-05 23:56:07 -0500

    adding line

--- Array_Length/Program.cs
+++ Array_Length/Program.cs
@@ -4,13 +4,13 @@ class Program
     static void Main()
     {
 
-        int[] arrayA = new int[10];
+        int[] arrayA = new int[35];
         int lengthA = arrayA.Length;
         Console.WriteLine("Length of ArrayA : {0}", +lengthA);
         long longLength = arrayA.LongLength;
         Console.WriteLine("Length of the LongLength Array  : {0}", longLength);
-       // int[,] twoD = new int[5, 10];
-       // Console.WriteLine("The Size of 2D Array is : {0}", twoD.Length);
-        //Console.ReadLine();
+        int[,] twoD = new int[34, 10];
+        Console.WriteLine("The Size of 2D Array is : {0}", twoD.Length);
+        Console.ReadLine();
     }
 }
