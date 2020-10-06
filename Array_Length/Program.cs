7d0baa2d174f0753cbb1296a088f126bd686efa3
Roopha <roophashree@gmail.com>
2020-10-05 23:59:06 -0500

    Values

--- Array_Length/Program.cs
+++ Array_Length/Program.cs
@@ -9,7 +9,7 @@ class Program
         Console.WriteLine("Length of ArrayA : {0}", +lengthA);
         long longLength = arrayA.LongLength;
         Console.WriteLine("Length of the LongLength Array  : {0}", longLength);
-        int[,] len = new int[34, 10];
+        int[,] len = new int[4, 1];
         Console.WriteLine("The Size of 2D Array is : {0}", len.Length);
         Console.ReadLine();
     }
