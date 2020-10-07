a087464dec16c0ce15680e0a1f9c62d8fdf8c322
Roopha <roophashree@gmail.com>
2020-10-06 22:39:46 -0500

    Reversal of array

---
+++ Array_reversal/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Array_reversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 4, 6, 8, 9, 0, 2, 3 };
+            foreach(int a in array)
+            {
+                Console.WriteLine(a);
+            }
+            Array.Reverse(array);
+            Console.WriteLine("Reversed Array is:");
+            foreach(int value in array)
+            {
+                Console.WriteLine(value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
