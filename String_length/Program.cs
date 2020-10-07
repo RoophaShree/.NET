0bc0745b6d7f02a718bf8e2fa5634294655026f8
Roopha <roophashree@gmail.com>
2020-10-06 22:46:31 -0500

    stringlength

---
+++ String_length/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace String_length
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "RoophaShree";
+            Console.WriteLine("The length of the first string is:" + s1.Length);
+            string s2 = " ";
+            Console.WriteLine("The length of thr second string is:" + s2.Length);
+           // Console.ReadLine();
+        }
+    }
+}
