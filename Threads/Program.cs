baa60d042d550295e41d8112808b13bd3df2e334
Roopha <roophashree@gmail.com>
2020-10-08 20:13:14 -0500

    MainThread

--- Threads/Program.cs
+++ Threads/Program.cs
@@ -9,7 +9,7 @@ namespace Threads
         {
             Thread t = Thread.CurrentThread;
             t.Name = "MainThread";
-            Console.WriteLine(t.Name);
+            Console.WriteLine("This is the main Thread inside a process:" +t.Name);
         }
     }
 }
