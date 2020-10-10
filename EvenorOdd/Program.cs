c2814e78346481151c6bd9d8cb5326871fcaf6c8
Roopha <roophashree@gmail.com>
2020-10-09 23:34:52 -0500

    edited code

--- EvenorOdd/Program.cs
+++ EvenorOdd/Program.cs
@@ -13,12 +13,12 @@ namespace EvenorOdd
             if (i % 2 == 0)
             {
                 Console.Write("Entered Number is an Even Number");
-                Console.Read();
+                //Console.Read();
             }
             else
             {
                 Console.Write("Entered Number is an Odd Number");
-                Console.Read();
+                //Console.Read();
             }
         }
     }
