fc02c3aacc2d1ffcba5e74abb323aab1c58938f4
Roopha <roophashree@gmail.com>
2020-10-10 22:51:42 -0500

    Commit

---
+++ Multiplication/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Multiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Enter 1st num=");
+            a = int.Parse(Console.ReadLine()); 
+            Console.WriteLine("Enter 2nd num=");
+            b = int.Parse(Console.ReadLine());
+
+            int result = a * b;
+
+            Console.WriteLine("Multiplication is :", +result);
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
