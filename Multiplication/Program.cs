a180d615b5b742ccfc96f4c7aec391cce01b5361
Roopha <roophashree@gmail.com>
2020-10-10 22:58:50 -0500

    Modified the code

--- Multiplication/Program.cs
+++ Multiplication/Program.cs
@@ -15,7 +15,7 @@ namespace Multiplication
             int result = a * b;
 
             Console.WriteLine("Multiplication output: {0} * {1} = {2}", a,b,result);
-            //Console.Readkey();
+            
 
 
 
