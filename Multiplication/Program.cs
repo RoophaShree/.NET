5b736bf4d2a131087c395eb4d811b7e50f8aec80
Roopha <roophashree@gmail.com>
2020-10-10 23:00:49 -0500

    3 numbers multiplication

--- Multiplication/Program.cs
+++ Multiplication/Program.cs
@@ -6,15 +6,17 @@ namespace Multiplication
     {
         static void Main(string[] args)
         {
-            int a, b;
+            int a, b, c;
             Console.WriteLine("Enter 1st num=");
             a = int.Parse(Console.ReadLine()); 
             Console.WriteLine("Enter 2nd num=");
             b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter 3rd num=");
+            c = int.Parse(Console.ReadLine());
 
-            int result = a * b;
+            int result = a * b*c;
 
-            Console.WriteLine("Multiplication output: {0} * {1} = {2}", a,b,result);
+            Console.WriteLine("Multiplication output: {0} * {1} * {2} = {3}", a,b,c,result);
             
 
 
