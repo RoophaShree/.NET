702c435e0d3b622f527ec20cedea068a276a03bb
Roopha <roophashree@gmail.com>
2020-10-11 23:34:46 -0500

    usd to inr

---
+++ USDtoINR/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace USDtoINR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double usd = 0;
+            double inr = 0;
+            double val = 0;
+
+            Console.WriteLine("Enter amt in USD:");
+            usd = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the USD Value: ");
+            val = double.Parse(Console.ReadLine());
+
+            inr = usd * val;
+            Console.WriteLine("USD " + usd +"---> INR " + inr);
+        }
+    }
+}
