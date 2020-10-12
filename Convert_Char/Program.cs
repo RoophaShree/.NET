29bc80ea87818156ef2e51c246ae28e21d5f1194
Roopha <roophashree@gmail.com>
2020-10-11 23:18:34 -0500

    case change

---
+++ Convert_Char/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Convert_Char
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch;
+
+            Console.Write("Enter a character : ");
+            ch = Convert.ToChar(Console.ReadLine());
+
+            if (ch >= 65 && ch <= 90)
+            {
+                Console.WriteLine("Convert Character '" + ch + "' into : '" + char.ToLower(ch) + "'");
+            }
+            else if (ch >= 97 && ch <= 122)
+            {
+                Console.WriteLine("Convert Character '" + ch + "' into : '" + char.ToUpper(ch) + "'");
+            }
+        }
+    }
+}
