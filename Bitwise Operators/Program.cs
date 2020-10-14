fe01f0915f991d757646d46e1f20d313e51e23b2
Roopha <roophashree@gmail.com>
2020-10-13 23:10:51 -0500

    Bitwise operator

---
+++ Bitwise Operators/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Bitwise_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 5;
+            int result = 0;
+
+            result=a & b;
+            result = a | b;     
+            Console.WriteLine("a | b  : {0}", result);
+
+            result = a ^ b;     
+            Console.WriteLine("a ^ b  : {0}", result);
+
+            result = ~a;        
+            Console.WriteLine("~a     : {0}", result);
+
+            result = a << 2;   
+            Console.WriteLine("a << b : {0}", result);
+
+            result = a >> 2;    
+            Console.WriteLine("a >> b : {0}", result);
+
+
+        }
+    }
+}
