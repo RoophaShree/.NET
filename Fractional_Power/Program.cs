ed77e426e916e816e3badd8f43ca9f0625f493eb
Roopha <roophashree@gmail.com>
2020-10-14 09:07:17 -0500

    Fractional_Power

---
+++ Fractional_Power/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Fractional_Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number1 = 0;
+            double number2 = 0;
+            double number3 = 0;
+
+            number1 = Math.Pow(Math.PI, 3.2);
+            number2 = Math.Pow(1.67, 1.8);
+            number3 = Math.Pow(Math.E, 1.7);
+
+            Console.WriteLine("Number1 : " + number1);
+            Console.WriteLine("Number2 : " + number2);
+            Console.WriteLine("Number3 : " + number3);
+        }
+    }
+}
