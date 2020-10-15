b6ecb4c24a6f1ae2ef25091bdbf6356204ec25bc
Roopha <roophashree@gmail.com>
2020-10-14 23:40:11 -0500

    Cube root

---
+++ Cube_root/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Cube_root
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            int number = 0;
+            double cubeRoot = 0;
+
+            Console.Write("Enter the value of number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            cubeRoot = Math.Ceiling(Math.Pow(number, (double)1 / 3));
+            Console.WriteLine("Cube Root is : " + cubeRoot);
+
+            return 0;
+        }
+    }
+}
