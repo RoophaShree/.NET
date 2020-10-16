9e03a19b65f8bfbcf634803452defd87b89c63ed
Roopha <roophashree@gmail.com>
2020-10-15 21:28:44 -0500

    using return

--- Cube_root/Program.cs
+++ Cube_root/Program.cs
@@ -4,7 +4,7 @@ namespace Cube_root
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
             int number = 0;
             double cubeRoot = 0;
@@ -15,7 +15,7 @@ namespace Cube_root
             cubeRoot = Math.Ceiling(Math.Pow(number, (double)1 / 3));
             Console.WriteLine("Cube Root is:" + cubeRoot);
 
-            //return 0;
+            return 0;
         }
     }
 }
