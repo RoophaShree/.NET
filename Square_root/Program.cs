5bea6d3b907a0e1c271ae68f4a5ae0450b1e9a5b
Roopha <roophashree@gmail.com>
2020-10-15 21:33:00 -0500

    using Parse

--- Square_root/Program.cs
+++ Square_root/Program.cs
@@ -10,7 +10,7 @@ namespace Square_root
             double squareRoot = 0;
 
             Console.Write("Enter the value of number: ");
-            number = Convert.ToInt32(Console.ReadLine());
+            number= int.Parse(Console.ReadLine());
 
             squareRoot = Math.Ceiling(Math.Pow(number, (double)1 / 2));
             Console.WriteLine("Square Root is : " + squareRoot);
