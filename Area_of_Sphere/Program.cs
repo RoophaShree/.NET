78fcaa34ac618cf00c68cdde30b584957332f3b0
Roopha <roophashree@gmail.com>
2020-10-16 23:01:00 -0500

    Area of sphere

---
+++ Area_of_Sphere/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Area_of_Sphere
+{
+    class Program
+    {
+        public static float CalculateArea(float radius)
+        {
+            float area = 0.0F;
+
+            area = (float)(4 * Math.PI * radius * radius);
+
+            return area;
+        }
+        public static void Main()
+        {
+            float radius = 0.0F;
+            float area = 0.0F;
+
+            Console.Write("Enter the value of radius: ");
+            radius = float.Parse(Console.ReadLine());
+
+            area = CalculateArea(radius);
+            Console.WriteLine("Area of Sphere: " + area);
+        }
+    }
+}
