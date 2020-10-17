2c1357245fb46dae805ed028e16d83d35e48f0e4
Roopha <roophashree@gmail.com>
2020-10-16 23:03:45 -0500

    vol of sphere

---
+++ Vol_of_Sphere/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Vol_of_Sphere
+{
+    class Program
+    {
+        public static float CalculateVolume(float radius)
+        {
+            float volume = 0.0F;
+
+            volume = (float)(4.0 / 3 * Math.PI * radius * radius * radius);
+
+            return volume;
+        }
+        public static void Main()
+        {
+            float radius = 0.0F;
+            float volume = 0.0F;
+
+            Console.Write("Enter the value of radius: ");
+            radius = float.Parse(Console.ReadLine());
+
+            volume = CalculateVolume(radius);
+            Console.WriteLine("Volume of Sphere: " + volume);
+        }
+}
