75cf64193d37b1f6bd295a52e03448cc0159b85f
Roopha <roophashree@gmail.com>
2020-10-17 23:29:24 -0500

    sqrfeet

---
+++ Size_of_the_area_in_sqrfeet/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Size_of_the_area_in_sqrfeet
+{
+    class Program
+    {
+        static int calculateArea(int lengthInFeets, int widthInFeets)
+        {
+            return (lengthInFeets * widthInFeets);
+        }
+        public static void Main()
+        {
+            int lengthInFeets = 0;
+            int widthInFeets = 0;
+            int plotArea = 0;
+
+
+            Console.Write("Enter length of the plot in feets: ");
+            lengthInFeets = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter width of room in feet:");
+            widthInFeets = Convert.ToInt32(Console.ReadLine());
+
+            plotArea = calculateArea(lengthInFeets, widthInFeets);
+
+            Console.WriteLine("Plot area is " + plotArea + " square feet");
+        }
+    }
+}
