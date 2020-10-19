de6459383956e7e247f1a04ec7af9728a5b60713
Roopha <roophashree@gmail.com>
2020-10-18 23:39:17 -0500

    sinvalue

---
+++ sin_Value/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace sin_Value
+{
+    class Program
+    {
+        static double CalculateSinX(int deg, int terms)
+        {
+            double x;
+            double result;
+            double temp;
+
+            int loop;
+            x = Math.PI * deg / 180f;
+            result = x;
+            temp = x;
+
+            for (loop = 1; loop <= terms; loop++)
+            {
+                temp = (-temp * x * x) / ((2 * loop) * (2 * loop + 1));
+                result = result + temp;
+            }
+
+            return result;
+        }
+        public static void Main()
+        {
+            int degree = 0;
+            int terms = 0;
+
+            double result = 0.0;
+
+            Console.Write("Enter the angle in Degrees:");
+            degree = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the number of terms:");
+            terms = int.Parse(Console.ReadLine());
+
+            result = CalculateSinX(degree, terms);
+            Console.WriteLine("Sin({0})={1}", degree, result);
+        }
+    }
+}
+    
+
+
