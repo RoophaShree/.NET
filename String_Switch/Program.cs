ddb7172a3bcfc2249a3da6df06f83350155a6666
Roopha <roophashree@gmail.com>
2020-10-19 23:27:51 -0500

    Switch_String

---
+++ String_Switch/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace String_Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string gender = "Male";
+
+            switch (gender)
+            {
+                case "Male":
+                    Console.WriteLine("He is male...");
+                    break;
+                case "Female":
+                    Console.WriteLine("She is female...");
+                    break;
+                default:
+                    Console.WriteLine("Default");
+                    break;
+            }
+
+            
+            Console.ReadLine();
+        }
+    }
+}
