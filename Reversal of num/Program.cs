3b902b84c0cb033ab43df0cb1603b9873ece4f7e
Roopha <roophashree@gmail.com>
2020-10-20 22:39:47 -0500

    Reversal of num

---
+++ Reversal of num/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Reversal_of_num
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			
+			
+				int a = 721, rev = 0, b;
+
+				
+				while (a != 0)
+				{
+					b = a % 10;        
+					rev = (rev * 10) + b;   
+					a = a / 10;             
+				}
+				Console.WriteLine("The reverse of the number is: " + rev);
+			}
+		}
+	}
