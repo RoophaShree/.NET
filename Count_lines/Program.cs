0200226cfe6c4d57b8906fdc143da7751c44bb9e
Roopha <roophashree@gmail.com>
2020-10-21 19:17:00 -0500

    Commit

---
+++ Count_lines/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Count_lines
+{
+    class Program
+    {
+        static int CountLines(string str)
+        {
+            int lines = 1;
+            int index = 0;
+            while (true)
+            {
+                index = str.IndexOf('\n', index);
+                if (index < 0)
+                    break;
+                lines++;
+                index++;
+            }
+            return lines;
+        }
+        static void Main()
+        {
+            string str = "Mumbai Indians\nDelhi Capitals\nRajsthan Royals";
+
+            int lines = 0;
+
+            Console.WriteLine("Lines:");
+            Console.WriteLine(str);
+            lines = CountLines(str);
+            Console.WriteLine("Total lines in a string: " + lines);
+        }
+
+    }
+}
