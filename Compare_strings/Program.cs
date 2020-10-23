f1bafdd8ec6c9271d1c29de51f18540c61d7ae9f
Roopha <roophashree@gmail.com>
2020-10-22 21:40:50 -0500

    Compare two strings

---
+++ Compare_strings/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Compare_strings
+{
+    class Program
+    {
+        static void Main()
+        {
+            string str = "Hello";
+
+            if (str.CompareTo("Hello") == 0)
+            {
+                Console.WriteLine("String is matched");
+            }
+            else
+            {
+                Console.WriteLine("String is not matched");
+            }
+
+
+            if (str.CompareTo("Hiii") == 0)
+            {
+                Console.WriteLine("String is matched");
+            }
+            else
+            {
+                Console.WriteLine("String is not matched");
+            }
+        }
+    }
+}
