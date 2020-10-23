12b602428f987aa4dbb742666224543f9f984832
Roopha <roophashree@gmail.com>
2020-10-22 21:20:12 -0500

    changed the range

--- Random_num_generator/Program.cs
+++ Random_num_generator/Program.cs
@@ -4,7 +4,7 @@ public class Program
     public static void Main()
     {
         Random r = new Random();
-        int genRand = r.Next(10, 50);
+        int genRand = r.Next(1,5);
         Console.WriteLine("Random Number = " + genRand);
     }
 }
