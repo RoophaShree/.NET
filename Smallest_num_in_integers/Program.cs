a2149396924810b6fb71d800be620d9b11d4fc2b
Roopha <roophashree@gmail.com>
2020-10-24 19:39:58 -0500

    Commit

--- Smallest_num_in_integers/Program.cs
+++ Smallest_num_in_integers/Program.cs
@@ -8,18 +8,19 @@ namespace Smallest_num_in_integers
         {
             int i = 0;
             int small = 0;
-            int[] a = new int[5];
+            int[] a = new int[6];
 
             Console.WriteLine("Enter the array elements: ");
             for(i=0;i<a.Length;i++)
             {
-                Console.WriteLine("The Elements are :\n");
+                Console.Write("Element[" + (i + 1) + "]: ");
+                //Console.WriteLine("The Elements are :\n");
                 a[i] = int.Parse(Console.ReadLine());
             }
-            small = a[i];
+            small = a[0];
             for(i=1;i<a.Length;i++)
             {
-                if (small > a[i]) ;
+                if (small < a[i]) ;
                 small = a[i];
             }
             Console.WriteLine("The smallest element among the array elements are:" + small);
