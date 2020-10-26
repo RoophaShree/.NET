96f1c4e9317009db335615725feff01af4b0c091
Roopha <roophashree@gmail.com>
2020-10-25 23:26:35 -0500

    IP_Address

---
+++ IP_Address/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+
+namespace IP_Address
+{
+    class Program
+    {
+        static void Main()
+        {
+            IPAddress[] ips;
+            IPHostEntry entry;
+            string hostName = "";
+
+            hostName = Dns.GetHostName();
+            Console.WriteLine("Hostname of computer: " + hostName);
+
+            entry = Dns.GetHostEntry(hostName);
+            ips = entry.AddressList;
+
+            for (int i = 0; i < ips.Length; i++)
+            {
+                Console.WriteLine("IP Address: " + ips[i]);
+            }
+        }
+    }
+}
