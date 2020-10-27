8fc73be48192901979198b2fb54b6cadc1576576
Roopha <roophashree@gmail.com>
2020-10-27 14:16:45 -0500

    with row val to 10

--- Start_Pattern/Program.cs
+++ Start_Pattern/Program.cs
@@ -21,7 +21,7 @@ namespace Start_Pattern
         }
     }
 }
-*/
+
 
 using System.IO;
 using System;
@@ -32,6 +32,25 @@ class Program
         for (int i = 6; i >= 1; --i)
         {
             for (int j = 1; j >= i; ++j)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+*/
+
+using System.IO;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 10; ++i)
+        {
+            for (int j = 1; j <= i; ++j)
             {
                 Console.Write("*");
             }
