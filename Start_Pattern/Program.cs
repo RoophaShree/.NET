a40fab263ae3b3896da3df0cacfb8e922877aa73
Roopha <roophashree@gmail.com>
2020-10-27 14:09:43 -0500

    * Pattern

---
+++ Start_Pattern/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Start_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int row = 0;
+            //int col = 0;
+
+            for (int row = 4; row >= 1; --row)
+            {
+                for (int col = 1; col <= 1; ++col)
+                {
+                    Console.WriteLine("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
