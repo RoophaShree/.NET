06f83ad0c03094883f0c4a1827d83142bdc5702f
Roopha <roophashree@gmail.com>
2020-10-27 14:54:56 -0500

    *

--- Next Star Patterns/Program.cs
+++ Next Star Patterns/Program.cs
@@ -7,7 +7,7 @@ namespace Next_Star_Patterns
         static void Main(string[] args)
         {
             int i, j;
-            for (i = 1; i <= 5; i++)
+            for (i = 5; i >= 1; i--)
             {
                 for (j = 1; j <= i; j++)
                 {
