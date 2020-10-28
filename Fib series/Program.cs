fe97d6131cd9de9cf11aa5d8ef36915d6de742a6
Roopha <roophashree@gmail.com>
2020-10-27 19:32:05 -0500

    With Write()

--- Fib series/Program.cs
+++ Fib series/Program.cs
@@ -9,11 +9,11 @@ namespace Fib_series
             int n1 = 0, n2 = 1, n3, number, i;
             Console.Write("Enter the elements:");
             number = int.Parse(Console.ReadLine());
-            Console.Write(n1 + " " + n2 + " ");
+            Console.WriteLine(n1 + " " + n2 + " ");
             for (i = 2; i < number; ++i)
             {
                 n3 = n1 + n2;
-                Console.Write(n3 + " ");
+                Console.WriteLine(n3 + " ");
                 n1 = n2;
                 n2 = n3;
             }
