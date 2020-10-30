b773fdc72f707c962032ed87e495d90a14352d06
Roopha <roophashree@gmail.com>
2020-10-29 21:43:47 -0500

    Inheritance

---
+++ Inheritance_concept/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace Inheritance_concept
+{
+    class Roopa
+    {
+
+
+        public string name;
+        public string subject;
+
+
+        public void readers(string name, string subject)
+        {
+            this.name = name;
+            this.subject = subject;
+            Console.WriteLine("Myself: " + name);
+            Console.WriteLine("My Favorite Subject is: " + subject);
+        }
+        class Masters : Roopa
+        {
+
+            public Masters()
+            {
+                Console.WriteLine("Masters");
+            }
+        }
+
+        class Program
+        {
+
+
+            static void Main(string[] args)
+            {
+
+                Masters m = new Masters();
+
+
+                m.readers("Roo", "C#");
+            }
+        }
+    }
+}
