25c8402defe8466fe170f4c059c6192540d40c48
Roopha <roophashree@gmail.com>
2020-11-01 23:19:07 -0600

    Cel to Faren

---
+++ celsius to faren/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace celsius_to_faren
+{
+    class Program
+    {
+
+        static double convertToFahrenheit(double celTemp)
+        {
+            double fTemp = 0;
+
+            fTemp = (celTemp * 9) / 5 + 32;
+
+            return fTemp;
+        }
+        static void Main(string[] args)
+        {
+            double celTemp = 0;
+            double ferTemp = 0;
+
+            Console.Write("Enter the value of temperature in Celsius(°C): ");
+            celTemp = double.Parse(Console.ReadLine());
+
+            ferTemp = convertToFahrenheit(celTemp);
+            Console.WriteLine("Fahrenheit temperature is(°F) : " + ferTemp);
+        }
+    }
+}
