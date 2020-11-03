8b191875899272989c540956b6cd5f1df0e8c14e
Roopha <roophashree@gmail.com>
2020-11-02 23:22:24 -0600

    #define

---
+++ Hash_define_preprocessor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Hash_define_preprocessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if PRINT_MSG_TYPE1
+            Console.WriteLine("Print message type1 on console screen");
+#endif
+
+#if PRINT_MSG_TYPE2
+            Console.WriteLine("Print message type2 on console screen");
+#endif
+        }
+    }
+}
+    
