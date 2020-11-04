cbb8518f3ae66d2b44af875d278166e7675af5d9
Roopha <roophashree@gmail.com>
2020-11-03 22:54:44 -0600

    adding i

--- Anonymous/Program.cs
+++ Anonymous/Program.cs
@@ -7,7 +7,9 @@ namespace Anonymous
         public delegate void Print(int value);
         public static void Main(string[] args)
         {
+            int i = 10;
             Print print = delegate (int val) {
+                val += i;
                 Console.WriteLine("Inside Anonymous method. Value: {0}", val);
             };
 
