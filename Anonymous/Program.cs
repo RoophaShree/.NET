180a24a15f28f6e55cc22bf834cc695e52b90d5f
Roopha <roophashree@gmail.com>
2020-11-03 22:51:13 -0600

    Anonymous merthod

---
+++ Anonymous/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Anonymous
+{
+    class Program
+    {
+        public delegate void Print(int value);
+        static void Main(string[] args)
+        {
+            Print print = delegate (int val) {
+                Console.WriteLine("Inside Anonymous method. Value: {0}", val);
+            };
+
+            print(100);
+        }
+    }
+}
