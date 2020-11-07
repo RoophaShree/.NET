f14f2ace62ef829847d11dda3c44094b1643065b
Roopha <roophashree@gmail.com>
2020-11-06 23:19:39 -0600

    factors of given num

---
+++ Factor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Factor
+{
+    class Program
+    {
+        static void PrintFactors(int number)
+        {
+            int iLoop = 0;
+            Console.WriteLine("The all factors of " + number + " are :");
+
+            for (iLoop = 1; iLoop <= number; iLoop++)
+            {
+                if (number % iLoop == 0)
+                {
+                    Console.Write(iLoop + " ");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int number = 0;
+            Console.Write("Enter an integer number: ");
+            number = int.Parse(Console.ReadLine());
+
+            PrintFactors(number);
+
+            Console.WriteLine();
+        }
+    }
+}
+    
