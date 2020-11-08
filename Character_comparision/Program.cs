76cc7f3ac3f97e71b68e833bcbcb0715c8d344e6
Roopha <roophashree@gmail.com>
2020-11-07 22:34:05 -0600

    comparision

---
+++ Character_comparision/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Character_comparision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch1;
+            char ch2;
+
+            
+            Console.Write("Enter a character: ");
+            ch1 = Console.ReadLine()[0];
+            Console.Write("Enter another character: ");
+            ch2 = Console.ReadLine()[0];
+
+            
+            if (ch1 == ch2)
+                Console.WriteLine("Input characters are the same");
+            else
+                Console.WriteLine("Input characters are not the same");
+
+            
+            Console.ReadLine();
+        }
+    }
+}
