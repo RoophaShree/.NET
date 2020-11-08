e27b94fb6c35149f207be1b82b9c7bc06d58f7a3
Roopha <roophashree@gmail.com>
2020-11-07 22:36:48 -0600

    commit

--- Character_comparision/Program.cs
+++ Character_comparision/Program.cs
@@ -11,7 +11,7 @@ namespace Character_comparision
 
             
             Console.Write("Enter a character: ");
-            ch1 = Console.ReadLine();
+            ch1 = Console.ReadLine()[0];
             Console.Write("Enter another character: ");
             ch2 = Console.ReadLine()[0];
 
