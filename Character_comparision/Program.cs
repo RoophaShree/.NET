9c473241ea7edc1d4300cefa841007542d849704
Roopha <roophashree@gmail.com>
2020-11-08 23:24:20 -0600

    changed the varaibles

--- Character_comparision/Program.cs
+++ Character_comparision/Program.cs
@@ -6,17 +6,17 @@ namespace Character_comparision
     {
         static void Main(string[] args)
         {
-            int ch1;
-            int ch2;
+            int a;
+            int b;
 
             
             Console.Write("Enter a character/number: ");
-            ch1 = Console.ReadLine()[0];
+            a = Console.ReadLine()[0];
             Console.Write("Enter another character/number: ");
-            ch2 = Console.ReadLine()[0];
+            b= Console.ReadLine()[0];
 
             
-            if (ch1 == ch2)
+            if (a == b)
                 Console.WriteLine("Entered inputs are the same");
             else
                 Console.WriteLine("Entered outputs are not the same");
