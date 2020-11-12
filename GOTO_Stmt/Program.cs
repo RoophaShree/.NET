1d6bca88bdfb024ced5f0d5e8c13c6f2c43fa40d
Roopha <roophashree@gmail.com>
2020-11-11 22:50:06 -0600

    Modified code

--- GOTO_Stmt/Program.cs
+++ GOTO_Stmt/Program.cs
@@ -9,7 +9,7 @@ namespace GOTO_Stmt
             int number = 0;
             int power = 0;
             int result = 0;
-            int choice = 0;
+           // int choice = 0;
        // LOOP_LABEL:
 
             Console.Write("Enter number: ");
@@ -23,7 +23,7 @@ namespace GOTO_Stmt
             Console.WriteLine("Result : " + result);
 
             //Console.WriteLine("Do you want to calculate power again?? Press 1 for Yes, Press 2 for No: ");
-            choice = int.Parse(Console.ReadLine());
+            //choice = int.Parse(Console.ReadLine());
 
             //if (choice == 1)
               //  goto LOOP_LABEL;
