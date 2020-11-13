9349d23f9255141c817c308e18a5c7b80df373e0
Roopha <roophashree@gmail.com>
2020-11-12 22:46:12 -0600

    Negative To Positive

--- Negative_to_Positive/Program.cs
+++ Negative_to_Positive/Program.cs
@@ -6,7 +6,7 @@ namespace Negative_to_Positive
     {
         static void Main(string[] args)
         {
-            int[] arr = { 10, -20, 30, -40, 50, -60, 70 };
+            int[] arr = { 10, -20, 30, -40, 50, -60, 70, -90};
             int loop = 0;
 
             for (loop = 0; loop < arr.Length; loop++)
