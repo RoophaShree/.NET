aedbd06eab1e1a54cf8dc36b07b012926a85d9db
Roopha <roophashree@gmail.com>
2020-11-12 22:44:19 -0600

    Negto positive

---
+++ Negative_to_Positive/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Negative_to_Positive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, -20, 30, -40, 50, -60, 70 };
+            int loop = 0;
+
+            for (loop = 0; loop < arr.Length; loop++)
+            {
+                if (arr[loop] < 0)
+                    arr[loop] = -arr[loop];
+            }
+
+            Console.WriteLine("Array elements after conversion:");
+            for (loop = 0; loop < arr.Length; loop++)
+            {
+                Console.Write(arr[loop] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
