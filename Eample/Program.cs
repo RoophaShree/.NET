3b29d9a2d842f790417997abb6e1c195818bb704
Roopha <roophashree@gmail.com>
2020-11-13 23:22:21 -0600

    Magnitude

---
+++ Eample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Eample
+{
+    class Program
+    {
+        public static int GetMagnitude(int num)
+        {
+            int magnitude = 0;
+
+            while (num > 0)
+            {
+                magnitude++;
+                num = num / 10;
+            }
+            return magnitude;
+        }
+        public static void Main()
+        {
+            int num = 34521;
+            int mag = 0;
+
+            mag = GetMagnitude(num);
+
+            Console.WriteLine("Magnitude: " + mag);
+        }
+    }
+    }
+
