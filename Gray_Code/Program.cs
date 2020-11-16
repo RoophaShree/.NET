3b80dabcad22cee791ae10e51933fa02f9395830
Roopha <roophashree@gmail.com>
2020-11-15 23:08:43 -0600

    Gary_COde

---
+++ Gray_Code/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Gray_Code
+{
+    class Program
+    {
+        public static int CreateGraycode(int num)
+        {
+            int gray = 0;
+
+            gray = num ^ (num >> 1);
+
+            return gray;
+        }
+
+        static void Main(string[] args)
+        {
+            int loop = 0;
+
+            Console.WriteLine("Number\tGray Code");
+
+            for (loop = 0; loop < 5; loop++)
+            {
+                Console.WriteLine(string.Format("{0}\t{1}", loop, Convert.ToString((int)CreateGraycode(loop), 2)));
+            }
+        }
+    }
+}
