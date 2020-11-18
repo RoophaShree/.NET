8a3a8c39a78141ba60a2b10489ee137b73014e95
Roopha <roophashree@gmail.com>
2020-11-17 22:24:52 -0600

    To calculate mean

---
+++ Mean/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mean
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> list = new List<double> { 11, 22, 33, 44, 55 };
+
+            double mean = 0;
+            double sum = 0;
+
+            int loop = 0;
+
+            for (loop = 0; loop < list.Count; loop++)
+            {
+                sum += list[loop];
+            }
+
+            mean = sum / (list.Count - 0);
+
+            Console.WriteLine("Mean: " + mean);
+        }
+    }
+    }
+
