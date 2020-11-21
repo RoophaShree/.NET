fa026320e96bd42171601d32162de3c0f96d1488
Roopha <roophashree@gmail.com>
2020-11-20 23:27:51 -0600

    To Insert

---
+++ Insert_an_element/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Insert_an_element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int pos = 0;
+            int item = 0;
+            int[] arr = new int[10];
+
+            //Read numbers into array
+            Console.WriteLine("Enter numbers : ");
+            for (i = 0; i < 5; i++)
+            {
+                Console.Write("Element[" + (i + 1) + "]: ");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+
+            Console.Write("Enter position : ");
+            pos = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter new item : ");
+            item = int.Parse(Console.ReadLine());
+        }
+    }
+}
