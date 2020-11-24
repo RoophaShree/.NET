e7b147e4282e6b3daa8ad7653aaac564bfb29b59
Roopha <roophashree@gmail.com>
2020-11-23 23:48:48 -0600

    Commit

---
+++ foreach/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = { 12, 45, 67, 879, 89 };
+            foreach (int item in data)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
