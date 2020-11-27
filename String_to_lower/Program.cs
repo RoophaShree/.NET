cca92d311d0e79ce60f7ceb0f324ea8695d0664d
Roopha <roophashree@gmail.com>
2020-11-26 22:12:15 -0600

    lower case

---
+++ String_to_lower/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace String_to_lower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String str1;
+            String str2;
+
+            Console.Write("Enter string : ");
+            str1 = Console.ReadLine();
+
+            str2 = str1.ToLower();
+
+            Console.WriteLine("Converted string is: " + str2);
+        }
+    }
+
+
+}
