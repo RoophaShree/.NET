6bb0d6cb82bd9f4fc1b88252ef23a7bdfd1ba22b
Roopha <roophashree@gmail.com>
2020-11-27 23:46:20 -0600

    Another

--- PhoneBook/Program.cs
+++ PhoneBook/Program.cs
@@ -12,7 +12,7 @@ class program
             Console.WriteLine("\t1:Add number to phone book");
             Console.WriteLine("\t2:Get number to phone book");
             Console.WriteLine("\t3:Exit");
-            Console.WriteLine("\n#################################");
+            Console.WriteLine("\n##################################");
             Console.WriteLine("\n\nEnter choice: ");
             int choice = Convert.ToInt32(Console.ReadLine());
 
