063229b7abd443ca542e5bf97c2ed184311f0f07
Roopha <roophashree@gmail.com>
2020-11-29 23:08:08 -0600

    Commit

---
+++ If_Processor/Program.cs
@@ -0,0 +1,15 @@
+﻿#define MY_MACRO
+using System;
+using System.Diagnostics;
+
+class Program
+{
+    public static void Main()
+    {
+#if (MY_MACRO)
+        Console.WriteLine("Macro is defined");
+#else
+            Console.WriteLine("Macro is not defined");
+#endif
+    }
+}
