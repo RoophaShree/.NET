e640147e006d42ff41b49378b5c84a11aae4b7f5
Roopha <roophashree@gmail.com>
2020-12-01 23:30:54 -0600

    Commit

--- Upper_Triangle/Program.cs
+++ Upper_Triangle/Program.cs
@@ -23,7 +23,7 @@ namespace Upper_Triangle
                 }
             }
 
-            Console.WriteLine("\nMatrix: ");
+            Console.WriteLine("\n Matrix: ");
             for (i = 0; i < row; i++)
             {
                 for (j = 0; j < col; j++)
