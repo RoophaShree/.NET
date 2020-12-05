20511257e53604ad8b3ed9fd8cb79a8b14123c69
Roopha <roophashree@gmail.com>
2020-12-04 23:21:52 -0600

    Commit

---
+++ sumofdigits/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace sumofdigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int a = 567, sum = 0, b;
+
+			
+			while (a != 0)
+			{
+				b = a % 10;       //extract a digit
+				sum = sum + b;   //adding the digits
+				a = a / 10;      //remained number
+			}
+			Console.WriteLine("The sum of the digits is: " + sum);
+		}
+	}
+}
+    
+
