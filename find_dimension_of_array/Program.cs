9e3f72567a532142e35358c9ef0019ff09042e3d
Roopha <roophashree@gmail.com>
2020-12-07 23:16:43 -0600

    Dimensions

---
+++ find_dimension_of_array/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace find_dimension_of_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] OneD = new int[10];
+            int[,] TwoD = new int[3, 3];
+            int[,,] ThreeD = new int[2, 3, 3];
+
+            Console.WriteLine("Dimensions of OneD array is   : " + OneD.Rank);
+            Console.WriteLine("Demensions of TwoD array is   : " + TwoD.Rank);
+            Console.WriteLine("Demensions of ThreeD array is : " + ThreeD.Rank);
+        }
+
+    }
+}
+
